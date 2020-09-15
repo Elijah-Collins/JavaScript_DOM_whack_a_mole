6b75ef8100d86b13a00d57c19a3df3b971929cd3
Elijah Collins <ecollins152@west-mec.org>
2020-09-15 15:24:02 -0700

    more java

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let lastHole;
 let timeUp = false;
 let score = 0;
 let countdown;
+let timeLimit = 20000;
 function pickRandomHole(holes) {
     const randomHole = Math.floor(Math.random() * holes.length);
     const hole = holes[randomHole];
@@ -40,8 +41,28 @@ function startGame() {
     setTimeout(function(){
         timeUp = true;
     }, timeLimit );
-    let startCountdown = setInterval;
+    let startCountdown = setInterval(function(){
+        countdown -= 1;
+        countdownBoard.textContent = countdown;
+        if (countdown < 0){
+            countdown = 0;
+            clearInterval(startCountdown);
+            countdownBoard.textContent = "Time's up!! Thank you for protecting our planet! This is the way!" 
+        }
+    }, 1000);
 }
 //step 4 adding event listeners//
+startButton.addEventListener('click', startGame);
 
+function whack(e){
+    score++;
+    this.style.backgroundImage = 'url("yoda2.png")';
+    this.style.pointerEvents = 'none';
+    setTimeout(() => {
+        this.style.backgroundImage = 'url("yoda1.png")';
+        this.style.pointerEvents = 'all';
+    }, 800);
+    scoreBoard.textContent = score;
+}
 //step 5 keep track of score with forEach//
+moles.forEach(mole => mole.addEventListener('click', whack));
